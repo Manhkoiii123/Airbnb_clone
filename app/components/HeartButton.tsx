381268cf085e6c371b912502161ce439fe496e80
padi-dev-manhtd <manhtd@paditech.com>
2024-08-10 15:29:17 +0700

    favorite listing

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useFavorite from "@/app/hooks/useFavorite";
 import { SafeUser } from "@/app/types";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
@@ -8,8 +9,10 @@ interface HeartButtonProps {
   currentUser?: SafeUser | null;
 }
 const HeartButton = ({ listingId, currentUser }: HeartButtonProps) => {
-  const hasFavourite = false;
-  const toggleFavorite = () => {};
+  const { hasFavorite, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
   return (
     <div
       onClick={toggleFavorite}
@@ -21,7 +24,7 @@ const HeartButton = ({ listingId, currentUser }: HeartButtonProps) => {
       />
       <AiFillHeart
         size={24}
-        className={hasFavourite ? "fill-rose-500 " : "fill-neutral-500/70"}
+        className={hasFavorite ? "fill-rose-500 " : "fill-neutral-500/70"}
       />
     </div>
   );
